129ecdd71d15855a8564f5a836e8d1528654c109
FX-Wood <fxwood2@gmail.com>
2019-04-04 16:30:01 -0700

    Adding solutions and tests

--- selection_sort.py
+++ selection_sort.py
@@ -1,2 +1,15 @@
-def selection_sort(array):
-    return None
+def selection_sort(arr):
+    # for each element in arr
+    for i in range(len(arr)):
+        # set minimum at the beginning
+        index_of_min = i
+        # look for any elements with a lower value
+        for j in range(i, len(arr)):
+            # if one is found
+            if arr[j] < arr[index_of_min]:
+            # save its index as the new minimum
+                index_of_min = j
+        # once the minimum is found
+        # swap it with i
+        arr[i], arr[index_of_min] = arr[index_of_min], arr[i]
+    return arr
--- test_selection_sort.py
+++ test_selection_sort.py
@@ -11,6 +11,26 @@ class TestSelectionSort(object):
     def test_on_len_1(self):
         assert selection_sort([1]) == [1]
     
+    def test_on_len_2(self):
+        arr = [2,1]
+        assert selection_sort(arr) == [1,2]
+
+    def test_on_len_3(self):
+        arr = [1,3,2]
+        assert selection_sort(arr) == [1,2,3]
+
+    def test_on_len_3_mixed(self):
+        arr = [10, 3, 14]
+        assert selection_sort(arr) == [3,10,14]
+
     def test_on_len_10(self):
         arr = [rand(0,100) for n in range(10)]
-        assert selection_sort == arr.sort()
+        sorted_arr = arr.copy()
+        sorted_arr.sort()
+        assert selection_sort(arr) == sorted_arr
+
+    def test_duplicate_data(self):
+        arr = [1,2,1,1,1,1]
+        sorted_arr = arr.copy()
+        sorted_arr.sort()
+        assert selection_sort(arr) == sorted_arr
